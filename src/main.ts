1e3f4faf99dec18e89e53219bcf51ded66c76335
hiroshimashu <hiroshimashu@gmail.com>
2020-10-27 22:55:07 +0900

    add: Function class

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 class Variable {
 	// TODO: add type to data
-	public data: any;
-	constructor(data: any) {
+	public data: Number;
+	constructor(data: Number) {
 		this.data = data;
 	}
+}
+
+abstract class Function {
+	__call__(input: Variable): Variable {
+		const x = input.data;
+		const y = this.forward(x);
+		const output = new Variable(y);
+		return output;
+	}
+
+	abstract forward(x: Number): Number;
 }
 
 export default Variable;
